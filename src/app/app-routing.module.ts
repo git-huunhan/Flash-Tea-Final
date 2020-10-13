edd60601ebb08fc0ac1b75a3f4ee0e86c2607b6c
HuuThanh81 <huynhlehuuthanh@gmail.com>
2020-10-13 22:01:05 +0700

    new-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,55 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'bill-user',
+    loadChildren: () => import('./bill-user/bill-user.module').then( m => m.BillUserPageModule)
+  },
+  {
+    path: 'cancel-order',
+    loadChildren: () => import('./cancel-order/cancel-order.module').then( m => m.CancelOrderPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
+  {
+    path: 'change-password',
+    loadChildren: () => import('./change-password/change-password.module').then( m => m.ChangePasswordPageModule)
+  },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./checkout/checkout.module').then( m => m.CheckoutPageModule)
+  },
+  {
+    path: 'checkout-success',
+    loadChildren: () => import('./checkout-success/checkout-success.module').then( m => m.CheckoutSuccessPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+  {
+    path: 'item-detail',
+    loadChildren: () => import('./item-detail/item-detail.module').then( m => m.ItemDetailPageModule)
+  },
+  {
+    path: 'log-in',
+    loadChildren: () => import('./log-in/log-in.module').then( m => m.LogInPageModule)
+  },
+  {
+    path: 'new-item',
+    loadChildren: () => import('./new-item/new-item.module').then( m => m.NewItemPageModule)
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
   }
+
 ];
 @NgModule({
   imports: [
---
+++ src/app/bill-user/bill-user.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-user',
+  templateUrl: './bill-user.page.html',
+  styleUrls: ['./bill-user.page.scss'],
+})
+export class BillUserPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
